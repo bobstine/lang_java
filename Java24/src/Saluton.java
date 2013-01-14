40c32eb31b2d4ee5b505a721923e0768efc9a345
Bob at Penn <stine@wharton.upenn.edu>
2013-01-14 15:26:19 -0500

    initial repository

---
+++ Java24/src/Saluton.java
@@ -0,0 +1,7 @@
+public class Saluton {
+    public static void main(String[] arguments) {
+        // program goes here
+      String greeting = "Hi there!";
+      System.out.println(greeting);
+    }
+}
